3c8674da026b2c87fcb053a34a49cc124ed80b51
Deepak636216 <nutandeepak144353@gmail.com>
2024-07-02 22:04:56 +0530

    commut changes for social media

--- src/Components/Home/HeaderIcons.jsx
+++ src/Components/Home/HeaderIcons.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const HeaderIcons = () => {
   return (
     <div className="home_social">
-        <a href="#" className='home_social-link' target='_blank'>
+        <a href="https://www.instagram.com/invites/contact/?igsh=9muezt6s2azb&utm_content=lue14om" className='home_social-link' target='_blank'>
             <i class="fa-brands fa-instagram"></i>
         </a>
-        <a href="#" className='home_social-link' target='_blank'>
+        <a href="https://github.com/Deepak636216" className='home_social-link' target='_blank'>
             <i class="fa-brands fa-github"></i>
         </a>
-        <a href="#" className='home_social-link' target='_blank'>
+        <a href="www.linkedin.com/in/maram-deepak-22b582227" className='home_social-link' target='_blank'>
             <i class="fa-brands fa-linkedin"></i>
         </a>
     </div>
